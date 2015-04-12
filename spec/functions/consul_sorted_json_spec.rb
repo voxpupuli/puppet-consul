c605b72897652c810887d37cfbadf1c621c41b38
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2015-04-12 12:52:35 +0200

    Escape backslach in rspec test for consul_sorted_json
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- spec/functions/consul_sorted_json_spec.rb
+++ spec/functions/consul_sorted_json_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 describe 'consul_sorted_json' do
   it { should run.with_params({'foo' => :undef}).and_return("{}\n") }
-  it { should run.with_params({'b' => 1, 'a' => 2, 'c' => 3}).and_return('{"a":2,"b":1,"c":3}\n')}
+  it { should run.with_params({'b' => 1, 'a' => 2, 'c' => 3}).and_return('{"a":2,"b":1,"c":3}\\n')}
 end
