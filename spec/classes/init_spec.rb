a631eda15b44137734a24c72a554948470f760e7
Justin Lambert <jlambert@eml.cc>
2015-06-12 08:44:02 -0600

    update default consul version

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -29,7 +29,7 @@ describe 'consul' do
     }}
     it { expect { should compile }.to raise_error(/is not a boolean/) }
   end
-  
+
   context 'When passing a non-bool as manage_service' do
     let(:params) {{
       :manage_service => 'hello'
@@ -72,7 +72,7 @@ describe 'consul' do
   end
 
   context "When installing via URL by default" do
-    it { should contain_staging__file('consul.zip').with(:source => 'https://dl.bintray.com/mitchellh/consul/0.5.0_linux_amd64.zip') }
+    it { should contain_staging__file('consul.zip').with(:source => 'https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip') }
   end
 
   context "When installing via URL by with a special version" do
@@ -127,7 +127,7 @@ describe 'consul' do
         'ui_dir'   => '/dir1/dir2',
       },
     }}
-    it { should contain_staging__file('consul_web_ui.zip').with(:source => 'https://dl.bintray.com/mitchellh/consul/0.5.0_web_ui.zip') }
+    it { should contain_staging__file('consul_web_ui.zip').with(:source => 'https://dl.bintray.com/mitchellh/consul/0.5.2_web_ui.zip') }
   end
 
   context "When installing UI via URL by with a special version" do
