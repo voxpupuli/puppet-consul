dec6d077009481373edfbd7a027e4633d6bfbad5
Adam S <asa188@sfu.ca>
2015-08-12 21:56:56 -0700

    Puppet 4.x screwed up versioncmp with their facts
    Using regex instead due to some weird bug and our rspec tests
    using versioncmp with $::puppetversion coming back as a float even
    when running .to_s against it.

--- manifests/install.pp
+++ manifests/install.pp
@@ -26,10 +26,8 @@ class consul::install {
       ## Suggest moving to using puppetlabs-transition module to do the service work
       ##  after it gets some patchwork and closer to release version
       #
-      # Puppetversion fact has to be in quotes due to versioncmp differences between Puppet 3.x and 4.x
-      $puppet_version = inline_template("<%= Facter.value('puppetversion').to_s %>")
       # This was done for puppet 3.x not supporting Ubuntu 15 and Fedora 22, and since this ruby line doesn't support site.pp... override of Service { provider => 'systemd' }
-      if versioncmp($puppet_version, 4.2) < 0 {
+      if $::puppetversion =~ /^4/ {
         $ruby_service_stop = $::operatingsystem ? {
           'fedora' => "ruby -r 'puppet' -e \"Puppet::Type.type(:service).newservice(:name => 'consul', :provider => '${consul::init_style}').provider.send('stop')\"",
           'ubuntu' => "ruby -r 'puppet' -e \"Puppet::Type.type(:service).newservice(:name => 'consul', :provider => '${consul::init_style}').provider.send('stop')\"",
