6a50294fb5c68e0aa60a26217f7d5a6d85ef5f8b
aj-jester <aj@a-j.me>
2015-08-21 20:20:31 -0400

    unit tests for pretty config

--- spec/functions/consul_sorted_json_spec.rb
+++ spec/functions/consul_sorted_json_spec.rb
@@ -1,13 +1,44 @@
 require 'spec_helper'
 
-describe 'consul_sorted_json' do
-  it { should run.with_params({'foo' => :undef}).and_return("{}") }
-  it { should run.with_params({'b' => 1, 'a' => 2, 'c' => 3}).and_return('{"a":2,"b":1,"c":3}')}
-  it { should run.with_params({
-    'w' => -1,
-    'x' => '8500',
-    'y' => '-8656',
-    'z' => 'foo bar 123 4 5 6',
-    }).and_return('{"w":-1,"x":8500,"y":-8656,"z":"foo bar 123 4 5 6"}')
-  }
+describe 'consul_sorted_json', :type => :puppet_function do
+
+  let(:test_hash){ { 'z' => 3, 'a' => '1', 'p' => '2', 's' => '-7' } }
+  before do
+    @json = subject.call([test_hash, true])
+  end
+  it "sorts keys" do
+    expect( @json.index('a') ).to be < @json.index('p')
+    expect( @json.index('p') ).to be < @json.index('s')
+    expect( @json.index('s') ).to be < @json.index('z')
+  end
+
+  it "prints pretty json" do
+    expect(@json.split("\n").size).to eql(test_hash.size + 2) # +2 for { and }
+  end
+
+  it "prints ugly json" do
+    json = subject.call([test_hash]) # pretty=false by default
+    expect(json.split("\n").size).to eql(1)
+  end
+
+  it "validate ugly json" do
+    json = subject.call([test_hash]) # pretty=false by default
+    expect(json).to match("{\"a\":1,\"p\":2,\"s\":-7,\"z\":3}")
+  end
+
+  context 'nesting' do
+
+    let(:nested_test_hash){ { 'z' => [{'l' => 3, 'k' => '2', 'j'=> '1'}],
+                              'a' => {'z' => '3', 'x' => '1', 'y' => '2'},
+                              'p' => [ '9','8','7'] } }
+    before do
+      @json = subject.call([nested_test_hash, true])
+    end
+
+    it "sorts nested hashes" do
+      expect( @json.index('x') ).to be < @json.index('y')
+      expect( @json.index('y') ).to be < @json.index('z')
+    end
+
+  end
 end
