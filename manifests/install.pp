b27a4a32c325240a43c86733bef270e9b23492d9
aj-jester <aj@a-j.me>
2015-08-23 21:02:15 -0400

    Upgrade via URL method

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,6 @@
-# == Class consul::intall
+# == Class consul::install
 #
-# Installs consule based in the parameters from init
+# Installs consul using either url or system package.
 #
 class consul::install {
 
@@ -15,64 +15,57 @@ class consul::install {
 
   case $consul::install_method {
     'url': {
-      if $::operatingsystem != 'darwin' {
-        ensure_packages(['unzip'], { 'before' => Staging::File['consul.zip'] })
-      }
-      staging::file { 'consul.zip':
-        source => $consul::real_download_url
-      } ->
-      staging::extract { 'consul.zip':
-        target  => $consul::bin_dir,
-        creates => "${consul::bin_dir}/consul",
-      } ->
-      file { "${consul::bin_dir}/consul":
-        owner => 'root',
-        group => 0, # 0 instead of root because OS X uses "wheel".
-        mode  => '0555',
+
+      consul::url { 'core':
+        ensure      => $consul::core_package_ensure,
+        url         => $consul::real_core_package_url,
+        extract_dir => $consul::bin_dir,
+        checksum    => $consul::core_package_checksum,
+        digest      => $consul::url_digest,
+        timeout     => $consul::url_timeout,
+        env_path    => $consul::url_env_path,
+        tmp_dir     => $consul::url_tmp_dir,
+        require     => File[$consul::data_dir],
       }
 
-      if ($consul::ui_dir and $consul::data_dir) {
-        if $::operatingsystem != 'darwin' {
-          Package['unzip'] -> Staging::Deploy['consul_web_ui.zip']
-        }
-        file { "${consul::data_dir}/${consul::version}_web_ui":
-          ensure => 'directory',
-          owner  => 'root',
-          group  => 0, # 0 instead of root because OS X uses "wheel".
-          mode   => '0755',
-        } ->
-        staging::deploy { 'consul_web_ui.zip':
-          source  => $consul::real_ui_download_url,
-          target  => "${consul::data_dir}/${consul::version}_web_ui",
-          creates => "${consul::data_dir}/${consul::version}_web_ui/dist",
-        } ->
-        file { $consul::ui_dir:
-          ensure => 'symlink',
-          target => "${consul::data_dir}/${consul::version}_web_ui/dist",
+      if $consul::ui_dir {
+        consul::url { 'ui':
+          ensure      => $consul::ui_package_ensure,
+          url         => $consul::real_ui_package_url,
+          extract_dir => $consul::data_dir,
+          checksum    => $consul::ui_package_checksum,
+          digest      => $consul::url_digest,
+          timeout     => $consul::url_timeout,
+          env_path    => $consul::url_env_path,
+          tmp_dir     => $consul::url_tmp_dir,
+          require     => [Consul::Url['core'], File[$consul::data_dir]],
         }
       }
+
     }
     'package': {
-      package { $consul::package_name:
-        ensure => $consul::package_ensure,
+      package { $consul::core_package_name:
+        ensure => $consul::core_package_ensure,
       }
 
       if $consul::ui_dir {
         package { $consul::ui_package_name:
           ensure  => $consul::ui_package_ensure,
-          require => Package[$consul::package_name]
+          require => Package[$consul::core_package_name]
         }
       }
 
       if $consul::manage_user {
-        User[$consul::user] -> Package[$consul::package_name]
+        User[$consul::user] -> Package[$consul::core_package_name]
       }
 
       if $consul::data_dir {
-        Package[$consul::package_name] -> File[$consul::data_dir]
+        Package[$consul::core_package_name] -> File[$consul::data_dir]
       }
     }
-    'none': {}
+    'none': {
+      #noop
+    }
     default: {
       fail("The provided install method ${consul::install_method} is invalid")
     }
