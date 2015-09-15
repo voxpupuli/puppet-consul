228b540fcf21368587aa9d1675f092d0f38e6cf9
bearrito <bstrausser@trafficland.com>
2015-09-15 09:54:48 -0400

    Remove trailing whitespace from second guard

--- manifests/install.pp
+++ manifests/install.pp
@@ -35,8 +35,8 @@ class consul::install {
 
       if ($consul::ui_dir and $consul::data_dir) {
         if $::operatingsystem != 'darwin' {
-          if ! defined ( Package['unzip'] ) { 
-            ensure_packages(['unzip'], { 'before' => Staging::File['consul.zip'] }) 
+          if ! defined ( Package['unzip'] ) {
+            ensure_packages(['unzip'], { 'before' => Staging::File['consul.zip'] })
           }
         }
         file { "${consul::data_dir}/${consul::version}_web_ui":
