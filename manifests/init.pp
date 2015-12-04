aaeac52bcce56722535f8aeef1114eda31c80b32
Daniel Hopper <daniel.hopper@cdk.com>
2015-12-04 11:29:33 -0500

    Removing keys from repository as they will be stored elsewhere

--- manifests/init.pp
+++ manifests/init.pp
@@ -176,9 +176,6 @@ class consul (
       purge       => $purge_config_dir,
       notify      => $notify_service,
     } ->
-    class { 'consul::keys_ssl':
-      package_target => $consul::params::package_target,
-    } ->
     class { 'consul::windows_service':
       package_target => $consul::params::package_target,
       service_name   => $consul::params::service_name,
@@ -191,10 +188,6 @@ class consul (
       config_hash => $config_hash_real,
       purge       => $purge_config_dir,
       notify      => $notify_service,
-    } ->
-    class { 'consul::keys_ssl':
-      package_target => $consul::params::package_target,
-      config_dir     => $consul::config_dir,
     } ->
     class { 'consul::run_service': } ->
     class { 'consul::reload_service': } ->
