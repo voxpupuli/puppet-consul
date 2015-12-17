6f189afe3ab6e1d58cb2ec9479653255fc982b39
Rob Rankin <github@undertow.ca>
2015-12-17 19:01:42 +0000

    Add Windows test

--- spec/unit/facter/consul_version_spec.rb
+++ spec/unit/facter/consul_version_spec.rb
@@ -3,19 +3,35 @@ require "spec_helper"
 describe Facter::Util::Fact do
   before {
     Facter.clear
-    allow(Facter.fact(:kernel)).to receive(:value).and_return("Linux")
   }
 
   describe "consul_version" do
+
+    context 'Returns consul version on Linux'
     it do
       consul_version_output = <<-EOS
 Consul v0.6.0
 Consul Protocol: 3 (Understands back to: 1)
       EOS
+      allow(Facter.fact(:kernel)).to receive(:value).and_return("Linux")
       allow(Facter::Util::Resolution).to receive(:exec).with('consul --version').
         and_return(consul_version_output)
       expect(Facter.fact(:consul_version).value).to match(/\d+\.\d+\.\d+/)
     end
+
+    context 'Returns consul version on Windows'
+    it do
+      consul_version_output = <<-EOS
+Consul v0.6.0
+Consul Protocol: 3 (Understands back to: 1)
+      EOS
+      allow(Facter.fact(:kernel)).to receive(:value).and_return("windows")
+      allow(Facter::Util::Resolution).to receive(:exec).with('"C:\\Program Files\\Consul\\consul.exe" --version').
+        and_return(consul_version_output)
+      expect(Facter.fact(:consul_version).value).to match(/\d+\.\d+\.\d+/)
+    end
+
+
   end
 
 end
