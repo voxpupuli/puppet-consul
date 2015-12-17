bfd9b771b2b862103f710c0baa133c5e094a2faf
Rob Rankin <github@undertow.ca>
2015-12-17 17:05:46 +0000

    Rewrite as ruby

--- lib/facter/consul_version.rb
+++ lib/facter/consul_version.rb
@@ -1,17 +1,15 @@
 # consul_version.rb
 
-Facter.add(:consul_version) do
-  confine :kernel => 'Linux'
-  setcode do
-    Facter::Core::Execution.exec('consul --version | head -1 | cut -f2 -d\ | cut -c2-')
-  end
+if Facter.value(:kernel) == 'Linux'
+  version = %x[consul --version]
+  version = version.lines.first.split[1].tr('v','')
+elsif Facter.value(:kernel) == 'windows'
+  version = %x["C:\\Program Files\\Consul\\consul.exe" --version]
+  version = version.lines.first.split[1].tr('v','')
 end
 
 Facter.add(:consul_version) do
-  confine :kernel => 'windows'
   setcode do
-    powershell = 'powershell'
-    command = ' (((&\"C:\Program Files\Consul\consul.exe\" --version)[0]).Split(\"Consul v\")[8]).trim() '
-    Facter::Util::Resolution.exec(%Q{#{powershell} -command "#{command}"})
+      version
   end
 end
