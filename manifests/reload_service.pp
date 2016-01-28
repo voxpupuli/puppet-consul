92360d91d3da25c5bc7a97ef8f7efc410cd1b7e2
Daniel Ben-Zvi <daniel.benzvi@gmail.com>
2016-01-28 13:45:07 +0200

    Linting

--- manifests/reload_service.pp
+++ manifests/reload_service.pp
@@ -11,14 +11,14 @@ class consul::reload_service {
   # This can happen during pre-provisioning of a node.
   if $consul::manage_service == true and $consul::service_ensure == 'running' {
 
-  	# Make sure we don't try to connect to 0.0.0.0, use 127.0.0.1 instead
-  	# This can happen if the consul agent RPC port is bound to 0.0.0.0
-  	if $consul::rpc_addr == '0.0.0.0' {
+    # Make sure we don't try to connect to 0.0.0.0, use 127.0.0.1 instead
+    # This can happen if the consul agent RPC port is bound to 0.0.0.0
+    if $consul::rpc_addr == '0.0.0.0' {
       $rpc_addr = '127.0.0.1'
-  	} else {
-	  $rpc_addr = $consul::rpc_addr  		
-  	}
-  	
+    } else {
+      $rpc_addr = $consul::rpc_addr
+    }
+
     exec { 'reload consul service':
       path        => [$consul::bin_dir,'/bin','/usr/bin'],
       command     => "consul reload -rpc-addr=${rpc_addr}:${consul::rpc_port}",
