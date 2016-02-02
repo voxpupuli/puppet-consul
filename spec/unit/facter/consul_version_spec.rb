dfbcf8f633267175b6315c2831952b5a703e2224
Rob Rankin <github@undertow.ca>
2016-02-02 14:43:41 +0000

    Corecting the test would be a good idea as well

--- spec/unit/facter/consul_version_spec.rb
+++ spec/unit/facter/consul_version_spec.rb
@@ -14,7 +14,7 @@ Consul v0.6.0
 Consul Protocol: 3 (Understands back to: 1)
       EOS
       allow(Facter.fact(:kernel)).to receive(:value).and_return("Linux")
-      allow(Facter::Util::Resolution).to receive(:exec).with('consul --version').
+      allow(Facter::Util::Resolution).to receive(:exec).with('consul --version 2> /dev/null').
         and_return(consul_version_output)
       expect(Facter.fact(:consul_version).value).to match('0.6.0')
     end
