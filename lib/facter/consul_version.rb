acb20f8441230fba9f77ecaf3e578e56b6c25099
Rob Rankin <github@undertow.ca>
2016-02-02 14:33:49 +0000

    Quiet any error output for nodes that dont have Consul installed

--- lib/facter/consul_version.rb
+++ lib/facter/consul_version.rb
@@ -3,7 +3,7 @@
 Facter.add(:consul_version) do
   confine :kernel => 'Linux'
   setcode do
-    version = Facter::Util::Resolution.exec('consul --version')
+    version = Facter::Util::Resolution.exec('consul --version 2> /dev/null')
     version = version.lines.first.split[1].tr('v','')
     version
   end
