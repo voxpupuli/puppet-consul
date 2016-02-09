f0099be0273a07f8a3e10e9b5d61bfdc98639d77
Lee Briggs <lee@leebriggs.co.uk>
2016-02-09 17:21:06 +0000

    Fix fact when consul isn't installed

--- lib/facter/consul_version.rb
+++ lib/facter/consul_version.rb
@@ -3,8 +3,8 @@
 Facter.add(:consul_version) do
   confine :kernel => 'Linux'
   setcode do
-    version = Facter::Util::Resolution.exec('consul --version 2> /dev/null')
-    version = version.lines.first.split[1].tr('v','')
+    version = Facter::Util::Resolution.exec('consul --version 2> /dev/null').lines.first.split[1].tr('v','')
+    version = version.lines.first.split[1].tr('v','') unless version.nil?
     version
   end
 end
