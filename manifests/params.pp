5d4415804c2e2fe6a3664b0a295fe2e0aaf64814
Gil Brechbuhler <gibre12@gmail.com>
2016-02-28 18:42:30 +0100

    add support for arm (Raspberry pi's ARM here) architecture

--- manifests/params.pp
+++ manifests/params.pp
@@ -20,6 +20,7 @@ class consul::params {
   case $::architecture {
     'x86_64', 'amd64': { $arch = 'amd64' }
     'i386':            { $arch = '386'   }
+    'armv61':          { $arch = 'arm'   }
     default:           {
       fail("Unsupported kernel architecture: ${::architecture}")
     }
