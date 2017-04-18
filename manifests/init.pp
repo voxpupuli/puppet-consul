1aad9e456ccb76a255d5efefa0d3f5a5bbbf610f
Christian Becker <christian.becker@wywy.com>
2017-04-18 18:36:49 +0200

    fix new lint arrow check

--- manifests/init.pp
+++ manifests/init.pp
@@ -254,14 +254,13 @@ class consul (
   }
 
   anchor {'consul_first': }
-  ->
-  class { 'consul::install': } ->
-  class { 'consul::config':
+  -> class { 'consul::install': }
+  -> class { 'consul::config':
     config_hash => $config_hash_real,
     purge       => $purge_config_dir,
     notify      => $notify_service,
-  } ->
-  class { 'consul::run_service': } ->
-  class { 'consul::reload_service': } ->
-  anchor {'consul_last': }
+  }
+  -> class { 'consul::run_service': }
+  -> class { 'consul::reload_service': }
+  -> anchor {'consul_last': }
 }
