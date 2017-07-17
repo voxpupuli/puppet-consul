1661be373940770b95548b9d34d047e6b60dbfbd
Kristian Jensen <krjensen@ebay.com>
2017-07-17 12:01:35 +0200

    Fixed linter

--- manifests/windows_service.pp
+++ manifests/windows_service.pp
@@ -1,4 +1,18 @@
-class consul::windows_service(
+# == Class consul::windows_service
+#
+# Installs consul windows server
+# == Parameters
+#
+# [*nssm_version*]
+#   nssm version to download
+#
+# [*nssm_download_url*]
+#   nssm version to download
+#
+# [*nssm_download_url_base*]
+#   nssm version to download
+#
+class consul::windows_service (
   $nssm_version = '2.24',
   $nssm_download_url = undef,
   $nssm_download_url_base = 'https://nssm.cc/release',
@@ -25,8 +39,8 @@ class consul::windows_service(
   $agent_args = regsubst($consul::config_dir, '\/', '\\', 'G')
   $app_args = "agent -config-dir=${agent_args}"
   $app_log_path = "${app_dir}\\logs"
-  $app_log_file = "consul.log"
-  $app_log = "$app_log_path//$app_log_file"
+  $app_log_file = 'consul.log'
+  $app_log = "${app_log_path}//${app_log_file}"
 
   include '::archive'
 
@@ -35,8 +49,8 @@ class consul::windows_service(
     owner  => $consul::user,
     group  => $consul::group,
     mode   => '0755',
-  } ->
-  archive { "${install_path}/nssm-${nssm_version}.zip":
+  }
+  -> archive { "${install_path}/nssm-${nssm_version}.zip":
     ensure       => present,
     source       => $real_download_url,
     extract      => true,
@@ -45,13 +59,13 @@ class consul::windows_service(
       "${install_path}/nssm-${nssm_version}/win32/nssm.exe",
       "${install_path}/nssm-${nssm_version}/win64/nssm.exe",
     ]
-  }->
-    file { "$nssm_exec":
+  }
+  -> file { $nssm_exec :
     owner => $consul::user,
     group => $consul::group,
     mode  => '0775',
-  }->
-  exec { 'consul_service_install':
+  }
+  -> exec { 'consul_service_install':
     cwd       => $consul::bin_dir,
     command   => "${nssm_exec} install Consul ${app_exec}",
     unless    => 'if((get-service -name consul -ErrorAction SilentlyContinue).count -ne 1){exit 1}',
@@ -63,8 +77,8 @@ class consul::windows_service(
     ensure  => 'present',
     content => template('consul/set_service_parameters.ps1.erb'),
     notify  => Exec['consul_service_set_parameters']
-  } ->
-  exec { 'consul_service_set_parameters':
+  }
+  -> exec { 'consul_service_set_parameters':
     cwd         => $consul::bin_dir,
     command     => "${consul::bin_dir}/set_service_parameters.ps1",
     refreshonly => true,
