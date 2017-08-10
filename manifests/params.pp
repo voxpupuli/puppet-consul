986abdda9e49d392205508ac8e2194000c4b8020
Justin King <jking@lanterncredit.com>
2017-08-09 22:22:59 -0700

    revert version default to 0.7.4

--- manifests/params.pp
+++ manifests/params.pp
@@ -41,7 +41,7 @@ class consul::params {
   $ui_package_ensure     = 'latest'
   $ui_package_name       = 'consul_ui'
   $user                  = 'consul'
-  $version               = '0.9.0'
+  $version               = '0.7.4'
   $watches               = {}
 
   case $::architecture {
