00835ed50b7d8817dc101cd8e52944867a70e609
David Leib <david.leib@barzahlen.de>
2017-11-14 17:55:08 +0100

    Removing the consul config check
    Originally this config check wasn't working with the newest consul version and a workaround with saving it temporarily was introduced. When updating the configuration this check wasn't working either so it is safer to remove it and leave it to the developer to set the correct information.

--- manifests/config.pp
+++ manifests/config.pp
@@ -106,14 +106,13 @@ class consul::config(
     recurse => $purge,
   }
   -> file { 'consul config.json':
-    ensure       => present,
-    path         => "${consul::config_dir}/config.json",
-    owner        => $::consul::user_real,
-    group        => $::consul::group_real,
-    mode         => $::consul::config_mode,
-    content      => consul_sorted_json($config_hash, $::consul::pretty_config, $::consul::pretty_config_indent),
-    require      => File[$::consul::config_dir],
-    validate_cmd => "${::consul::bin_dir}/consul validate $(ln -sf % /tmp/consul-config.json && echo \"/tmp/consul-config.json\") > /dev/null",
+    ensure  => present,
+    path    => "${consul::config_dir}/config.json",
+    owner   => $::consul::user_real,
+    group   => $::consul::group_real,
+    mode    => $::consul::config_mode,
+    content => consul_sorted_json($config_hash, $::consul::pretty_config, $::consul::pretty_config_indent),
+    require => File[$::consul::config_dir],
   }
 
 }
