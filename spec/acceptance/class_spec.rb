7206a2d957312fe6eecadae34833a52e6e0c3976
Tim Meusel <tim@bastelfreak.de>
2018-05-12 19:42:26 +0200

    add another acceptance test

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -12,8 +12,6 @@ describe 'consul class' do
           manage_service => true,
           config_hash    => {
               'datacenter' => 'east-aws',
-              'data_dir'   => '/opt/consul',
-              'log_level'  => 'INFO',
               'node_name'  => 'foobar',
               'server'     => true,
           }
@@ -38,4 +36,41 @@ describe 'consul class' do
     end
 
   end
+  context 'default parameters' do
+    it 'should work with no errors based on the example' do
+      pp = <<-EOS
+        package { 'zip': ensure => present } ->
+        # Don't manage the service as it doesn't work well in docker
+        class { 'consul':
+          version        => '1.1.0',
+          manage_service => true,
+          config_hash    => {
+              'datacenter' => 'east-aws',
+              'data_dir'   => '/opt/consul',
+              'log_level'  => 'INFO',
+              'node_name'  => 'foobar',
+              'server'     => true,
+          }
+        }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
+    end
+
+    describe file('/opt/consul') do
+      it { should be_directory }
+    end
+
+    describe service('consul') do
+      it { should be_enabled }
+    end
+
+    describe command('consul version') do
+      its(:stdout) { should match %r{Consul v1.1.0} }
+    end
+
+  end
+
 end
