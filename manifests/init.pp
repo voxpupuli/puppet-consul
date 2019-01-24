602985fcf2b125192dcb69e774c9e9f2813ee754
krjensen <krjensen@ebay.com>
2019-01-24 09:46:57 +0100

    Update from master

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,17 +9,6 @@
 #
 # [*arch*]
 #   Architecture of consul binary to download.
-# == Class: consul
-#
-# Installs, configures and manages consul
-#
-# === Parameters
-#
-# [*acls*]
-#   Hash of consul_acl resources to create.
-#
-# [*arch*]
-#   Architecture of consul binary to download.
 #
 # [*archive_path*]
 #   Path used when installing consul via the url.
@@ -97,9 +86,6 @@
 #
 # [*manage_user*]
 #   Whether to create/manage the user that should own consul's configuration files.
-#
-# [*nssm_exec*]
-#   Location of nssm windows binary for service management
 #
 # [*os*]
 #   OS component in the name of the archive file containing the consul binary.
@@ -198,7 +184,6 @@ class consul (
   Boolean $manage_group                      = $consul::params::manage_group,
   Boolean $manage_service                    = $consul::params::manage_service,
   Boolean $manage_user                       = $consul::params::manage_user,
-  Optional[String] $nssm_exec                = undef,
   $os                                        = $consul::params::os,
   $package_ensure                            = $consul::params::package_ensure,
   $package_name                              = $consul::params::package_name,
