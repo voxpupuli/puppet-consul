9a18e19368e1a543c266b76fd0f7a996950c9ddc
Tim Meusel <tim@bastelfreak.de>
2019-01-26 23:06:26 +0100

    Purge unused env vars

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,8 +3,8 @@ require 'beaker-puppet'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 
-run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
-install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
+run_puppet_install_helper
+install_ca_certs
 install_module_on(hosts)
 install_module_dependencies_on(hosts)
 
