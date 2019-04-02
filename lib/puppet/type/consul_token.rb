39a7b29e62ea1a9df5d3169c9df0cd519e650e74
Marius Meissner <marius.meissner@sixt.com>
2019-04-02 15:15:58 +0200

    acl_v2: Implemented new ACL system

---
+++ lib/puppet/type/consul_token.rb
@@ -0,0 +1,81 @@
+Puppet::Type.newtype(:consul_token) do
+
+  desc <<-'EOD'
+  Manages a v2 Consul token
+  EOD
+  ensurable
+
+  newparam(:name, :namevar => true) do
+    desc 'Name of the token'
+    validate do |value|
+      raise ArgumentError, "Token name must be a string" if not value.is_a?(String)
+    end
+  end
+
+  newparam(:accessor_id) do
+    desc 'Accessor ID of the token'
+    validate do |value|
+      raise ArgumentError, "Accessor ID must be a string" if not value.is_a?(String)
+    end
+  end
+
+  newparam(:policies_by_name) do
+    desc 'List of policy names assigned to the token'
+    validate do |value|
+      raise ArgumentError, "Policy name list must be an array" if not value.is_a?(Array)
+    end
+
+    defaultto []
+  end
+
+  newparam(:policies_by_id) do
+    desc 'List of policy IDs assigned to the token'
+    validate do |value|
+      raise ArgumentError, "Policy ID list must be an array" if not value.is_a?(Array)
+    end
+
+    defaultto []
+  end
+
+  newparam(:acl_api_token) do
+    desc 'Token for accessing the ACL API'
+    validate do |value|
+      raise ArgumentError, "ACL API token must be a string" if not value.is_a?(String)
+    end
+    defaultto ''
+  end
+
+  newproperty(:protocol) do
+    desc 'consul protocol'
+    newvalues(:http, :https)
+    defaultto :http
+  end
+
+  newparam(:port) do
+    desc 'consul port'
+    defaultto 8500
+    validate do |value|
+      raise ArgumentError, "The port number must be a number" if not value.is_a?(Integer)
+    end
+  end
+
+  newparam(:hostname) do
+    desc 'consul hostname'
+    validate do |value|
+      raise ArgumentError, "The hostname must be a string" if not value.is_a?(String)
+    end
+    defaultto 'localhost'
+  end
+
+  newparam(:api_tries) do
+    desc 'number of tries when contacting the Consul REST API'
+    defaultto 3
+    validate do |value|
+      raise ArgumentError, "Number of API tries must be a number" if not value.is_a?(Integer)
+    end
+  end
+
+  autorequire(:service) do
+    ['consul']
+  end
+end
---
+++ spec/unit/puppet/type/consul_token_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+
+describe Puppet::Type.type(:consul_token) do
+
+  it 'should fail if no name is provided' do
+    expect do
+      Puppet::Type.type(:consul_token).new(:type => 'client')
+    end.to raise_error(Puppet::Error, /Title or name must be provided/)
+  end
+
+  it 'should fail if accessor_id ist not a string' do
+    expect do
+      Puppet::Type.type(:consul_token).new(:name => 'foo', :accessor_id => {})
+    end.to raise_error(Puppet::Error, /ID must be a string/)
+  end
+
+  it 'should fail if policy name list is not an array' do
+    expect do
+      Puppet::Type.type(:consul_token).new(:name => 'foo', :policies_by_name => 'abc')
+    end.to raise_error(Puppet::Error, /Policy name list must be an array/)
+  end
+
+  it 'should fail if policy ID list is not an array' do
+    expect do
+      Puppet::Type.type(:consul_token).new(:name => 'foo', :policies_by_id => 'abc')
+    end.to raise_error(Puppet::Error, /Policy ID list must be an array/)
+  end
+
+  context 'with name defined' do
+    policies_by_name = ['test_1' 'test_2']
+    policies_by_id = ['abc-123' 'xyz-456']
+
+    before :each do
+      @token = Puppet::Type.type(:consul_token).new(
+        :name => 'testing',
+        :accessor_id => '39c75e12-7f43-0a40-dfba-9aa3fcda08d4',
+        :policies_by_name => policies_by_name,
+        :policies_by_id => policies_by_id,
+        )
+    end
+
+    it 'should accept a accessor id' do
+      expect(@token[:accessor_id]).to eq('39c75e12-7f43-0a40-dfba-9aa3fcda08d4')
+    end
+
+    it 'should accept policy names' do
+      expect(@token[:policies_by_name]).to eq(policies_by_name)
+    end
+
+    it 'should accept policy IDs' do
+      expect(@token[:policies_by_id]).to eq(policies_by_id)
+    end
+
+    it 'should default to localhost' do
+      expect(@token[:hostname]).to eq('localhost')
+    end
+
+    it 'should default to http' do
+      expect(@token[:protocol]).to eq(:http)
+    end
+
+    it 'should default to port 8500' do
+      expect(@token[:port]).to eq(8500)
+    end
+  end
+end
