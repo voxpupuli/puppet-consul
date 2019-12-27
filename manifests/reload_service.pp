d59d813838c0027b5857f6b5858da7e4319cf002
Michael Boutillier <michael.boutillier@ingenico.com>
2019-12-27 21:14:44 +0100

    fix(reload_service): remove typo in reload_options

--- manifests/reload_service.pp
+++ manifests/reload_service.pp
@@ -27,7 +27,7 @@ class consul::reload_service {
       $reload_options = "-http-addr=https://localhost:${consul::https_port} -client-cert=${consul::cert_file} -client-key=${consul::key_file}"
     }
     else {
-      $reload_options = "-http-addr=https://localhost:${consul::https_port}}"
+      $reload_options = "-http-addr=https://localhost:${consul::https_port}"
     }
 
 
