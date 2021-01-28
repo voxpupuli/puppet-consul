023774abbe5d86cb7052aca72842ac4bfd66ac59
Toby Lawrence <tobz@users.noreply.github.com>
2021-01-27 20:40:05 -0500

    Update validation for service checks to support gRPC health checks.

--- lib/puppet/functions/consul/validate_checks.rb
+++ lib/puppet/functions/consul/validate_checks.rb
@@ -15,28 +15,27 @@ Puppet::Functions.create_function(:'consul::validate_checks') do
             validate_checks(c)
         end
         when Hash
-            if ( (obj.key?("http") || ( obj.key?("script") || obj.key?("args") ) || obj.key?("tcp")) && (! obj.key?("interval")) )
-            raise Puppet::ParseError.new('interval must be defined for tcp, http, and script checks')
+            is_http = obj.key?("http")
+            is_tcp = obj.key?("tcp")
+            is_grpc = obj.key?("grpc")
+            is_script = obj.key?("script") || obj.key?("args")
+            is_ttl = obj.key?("ttl")
+            has_interval = obj.key?("interval")
+
+            check_types_defined = [is_http, is_tcp, is_grpc, is_script, is_ttl].count(true)
+
+            if check_types_defined > 1
+                raise Puppet::ParseError.new('multiple check types cannot be specified in a single check definition')
+            elsif check_types_defined == 0
+                raise Puppet::ParseError.new('One of the following check types must be defined: http, tcp, grpc, script, or ttl')
             end
 
-            if obj.key?("ttl")
-            if (obj.key?("http") || ( obj.key?("args") || obj.key?("script") ) || obj.key?("tcp") || obj.key?("interval"))
-                raise Puppet::ParseError.new('script, http, tcp, and interval must not be defined for ttl checks')
+            if (is_http || is_script || is_tcp || is_grpc) && !has_interval
+                raise Puppet::ParseError.new('interval must be defined for tcp, http, grpc, and script checks')
             end
-            elsif obj.key?("http")
-            if (( obj.key?("args") || obj.key?("script") ) || obj.key?("tcp"))
-                raise Puppet::ParseError.new('script and tcp must not be defined for http checks')
-            end
-            elsif obj.key?("tcp")
-            if (obj.key?("http") || ( obj.key?("args") || obj.key?("script") ))
-                raise Puppet::ParseError.new('script and http must not be defined for tcp checks')
-            end
-            elsif ( obj.key?("args") || obj.key?("script") )
-            if (obj.key?("http") || obj.key?("tcp"))
-                raise Puppet::ParseError.new('http and tcp must not be defined for script checks')
-            end
-            else
-            raise Puppet::ParseError.new('One of ttl, script, tcp, or http must be defined.')
+
+            if is_ttl && has_interval
+                raise Puppet::ParseError.new('interval must not be defined for ttl checks')
             end
         else
         raise Puppet::ParseError.new("Unable to handle object of type <%s>" % obj.class.to_s)
